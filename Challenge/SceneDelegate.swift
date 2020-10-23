512755dccfcd3c68c55db91e4e916f47346e870b
Luis Chávez <darkaw_remse@yahoo.com>
2020-10-23 05:13:52 -0500

    Change initialview

--- Challenge/SceneDelegate.swift
+++ Challenge/SceneDelegate.swift
@@ -6,18 +6,30 @@
 //
 
 import UIKit
-
+class CustomWindow : UIWindow {
+    //...
+}
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  
+    var window: CustomWindow!
+    var identifier: String!
+        func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
-    var window: UIWindow?
+            oUser.loadUserData()
+            if(oUser.login == false){
+                identifier = "CreateUserViewController"
+            }else{
+                identifier = "AudioViewController"
+            }
+            guard let windowScene = (scene as? UIWindowScene) else { return }
 
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let initialViewController = storyboard.instantiateViewController(withIdentifier: identifier)
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
-    }
+            window = CustomWindow(windowScene: windowScene)
+            window.rootViewController = initialViewController
+            window.makeKeyAndVisible()
+        }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
